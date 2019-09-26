d2902b6054b0edc51cc498381b42cbe79743cc4a
Kripa0047 <kripabairagi0047@gmail.com>
2019-09-26 19:56:58 +0530

    Updates

---
+++ precache-manifest.99ce4bba40ad93b9c1a5950c38cb785d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97822b251109f2a40708a6bb54bf5b7",
+    "url": "/youandme/index.html"
+  },
+  {
+    "revision": "b9bc409e92c284b15943",
+    "url": "/youandme/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "adf352b87cf3849b02d7",
+    "url": "/youandme/static/js/2.64b48cb6.chunk.js"
+  },
+  {
+    "revision": "b9bc409e92c284b15943",
+    "url": "/youandme/static/js/main.16b1791a.chunk.js"
+  },
+  {
+    "revision": "a250f066fc19d9e19a39",
+    "url": "/youandme/static/js/runtime-main.d0b828e2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/youandme/static/media/logo.25bf045c.svg"
+  }
+]);
